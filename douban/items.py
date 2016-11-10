0f9a91ebf201041fff7e48856ff0830486355eda
Laucr <leo.lau.inami@gmail.com>
2016-11-10 17:15:30 +0800

    Completed class DoubanBooksSpider & xpath expression. No test & debug.

--- douban/items.py
+++ douban/items.py
@@ -11,4 +11,7 @@ import scrapy
 class DoubanItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
-    pass
+    title = scrapy.Item()
+    intro = scrapy.Item()
+    sort = scrapy.Item()
+    _id = scrapy.Item()
--- douban/spiders/__init__.py
+++ douban/spiders/__init__.py
@@ -1,4 +1,14 @@
-# This package will contain the spiders of your Scrapy project
-#
-# Please refer to the documentation for information on how to create and manage
-# your spiders.
+# -*- coding: utf-8 -*-
+# __author__ = "Lau"
+
+
+def parse_a_book(response, _xpath):
+    sel = response.xpath('//' + _xpath)
+    _l = {'title': [],
+          '_id': []}
+    for a_sel in sel:
+        title = a_sel.xpath('.//' + '/text()').extract()
+        _id = a_sel.xpath('.//' + '/@href').extract().split('/')[-1]
+        _l['title'].append(title)
+        _l['_id'].append(_id)
+        return _l
