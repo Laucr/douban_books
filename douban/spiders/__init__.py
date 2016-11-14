3589b56fd78c02e68d42abec04702ebdfd39393f
Laucr <leo.lau.inami@gmail.com>
2016-11-14 11:18:21 +0800

    Completed class DoubanBooksSpider & xpath expression. No test & debug.

--- douban/spiders/__init__.py
+++ douban/spiders/__init__.py
@@ -3,12 +3,12 @@
 
 
 def parse_a_book(response, _xpath):
-    sel = response.xpath('//' + _xpath)
-    _l = {'title': [],
-          '_id': []}
+    sel = response.xpath(_xpath)
+    _l = []
     for a_sel in sel:
-        title = a_sel.xpath('.//' + '/text()').extract()
-        _id = a_sel.xpath('.//' + '/@href').extract().split('/')[-1]
-        _l['title'].append(title)
-        _l['_id'].append(_id)
-        return _l
+        pair = {'title': a_sel.xpath('/text()').extract(),
+                '_id': a_sel.xpath('/@href').extract()}
+        print a_sel.xpath('/@href').extract()
+        _l.append(pair)
+
+    return _l
--- douban/spiders/books_spider.py
+++ douban/spiders/books_spider.py
@@ -5,9 +5,6 @@ import sys
 
 import scrapy
 
-from __init__ import parse_a_book
-from douban.items import DoubanItem
-
 sys.path.append('..')
 
 reload(sys)
@@ -25,19 +22,35 @@ class DoubanBooksSpider(scrapy.spiders.Spider):
     ]
 
     # OVERRIDE
+    def start_requests(self):
+        yield scrapy.Request("https://book.douban.com/tag/童话",
+                             headers={"User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64; rv:48.0) "
+                                                    "\Gecko/20100101 Firefox/48.0"})
+
+    # OVERRIDE
     def parse(self, response):
         # DEBUG
         self.log('--- A response from %s just arrived.' % response.url)
 
         items = []
-        objs = parse_a_book(response, _xpath='div[@class="info"]/h2/a')
+        # objs = parse_a_book(response, _xpath='//div[@class="info"]/h2/a')
+
+        sel = scrapy.Selector(response)
+        texts = sel.xpath('//div[@class="info"]/h2/a')
+        _l = []
+        for a_sel in sel:
+            pair = {'title': a_sel.xpath('/text()').extract(),
+                    '_id': a_sel.xpath('/@href').extract()}
+            print a_sel.xpath('/@href').extract()
+            _l.append(pair)
+
         # //div[@id="link-report"]//p//span[@class="all hidden"][1]//div[@class="intro"]//p intro
-        for _iter_i in range(len(objs)):
-            book_size = len(objs[_iter_i]['title'][0])
-            for _iter_j in range(book_size):
-                item = DoubanItem
-                item['title'] = objs[_iter_i]['title'][0][_iter_j]
-                item['_id'] = objs[_iter_i]['_id'][0][_iter_j]
-                items.append(item)
-                yield item
+        # for _iter_i in range(len(objs)):
+        #     book_size = len(objs[_iter_i]['title'][0])
+        #     for _iter_j in range(book_size):
+        #         item = DoubanItem
+        #         item['title'] = objs[_iter_i]['title'][0][_iter_j]
+        #         item['_id'] = objs[_iter_i]['_id'][0][_iter_j]
+        #         items.append(item)
+        #         yield item
         print len(items)
