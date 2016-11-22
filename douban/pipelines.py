39469b9f4254a0c04456e28c95ac1b62602b6223
Laucr <leo.lau.inami@gmail.com>
2016-11-22 11:31:55 +0800

    File pipeline, no test/debug.

--- douban/pipelines.py
+++ douban/pipelines.py
@@ -5,7 +5,26 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
 
+import os
+import sys
 
-class DoubanPipeline(object):
+reload(sys)
+sys.setdefaultcoding('utf-8')
+
+
+class FilePipeline(object):
     def process_item(self, item, spider):
+        title = item['title']
+        intro = item['intro']
+        category = item['category']
+        file_root_dir = 'book_intro'
+        category_dir = os.path.join(file_root_dir, category)
+        if not os.path.exists(file_root_dir):
+            os.mkdir(file_root_dir)
+        if not os.path.exists(category_dir):
+            os.mkdir(category_dir)
+        if intro is not '':
+            with open(category_dir + '/' + title + '.txt', 'w') as f:
+                f.write(intro)
+                f.close()
         return item
